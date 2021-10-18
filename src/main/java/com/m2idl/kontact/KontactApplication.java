0e280e3c41ba0d3f7119309d84d9234561648eb5
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-10-18 11:21:49 +0200

    Init project

---
+++ src/main/java/com/m2idl/kontact/KontactApplication.java
@@ -0,0 +1,13 @@
+package com.m2idl.kontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2idl/kontact/KontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2idl.kontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
