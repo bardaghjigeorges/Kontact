bfa9f5a3260c5968c81f58ff40635209f7cfccb7
belHak <hak721@outlook.fr>
2021-10-20 22:00:36 +0200

    adding : security config

---
+++ src/main/java/com/m2idl/kontact/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.m2idl.kontact.config;
+
+import com.m2idl.kontact.service.UserAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserAuthenticationService userAuthenticationService ;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userAuthenticationService);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout();
+    }
+
+
+}
