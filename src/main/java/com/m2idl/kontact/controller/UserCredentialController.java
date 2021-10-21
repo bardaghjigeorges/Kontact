bba30a1488f5535e7f04826300e94552b8ff01e3
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-10-21 13:16:35 +0200

    creation des methodes "addUser", "finUserOptional", "findUser"

--- src/main/java/com/m2idl/kontact/controller/UserCredentialController.java
+++ src/main/java/com/m2idl/kontact/controller/UserCredentialController.java
@@ -5,20 +5,36 @@ import com.m2idl.kontact.service.UserCredentialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class UserCredentialController {
-/*
+
     @Autowired
     UserCredentialService userCredentialService;
 
-    @PostMapping("/index/addUser")
-    public UserCredential addNewUser(UserCredential userCredential){
+    @RequestMapping("/index/addUser")
+    public String addNewUser(UserCredential userCredential, Model model){
         userCredentialService.addUser(userCredential);
-        return userCredential;
-    }*/
+        model.addAttribute( "no,m_du_model_dans_la_vue", userCredential);
+        return "add_user_View";
+    }
+
+    @RequestMapping("/index/user/{email}/{password}")
+    public String findUserOptional(@PathVariable String email, @PathVariable String password, Model model ){
+        Optional<UserCredential> userCredential = userCredentialService.findOptionalUserByEmailAndPassword(email, password);
+        model.addAttribute( "nom_du_model_dans_la_vue", userCredential);
+        return "view";
+    }
+
+    @RequestMapping("/index/user/{email}/{password}")
+    public String findUser(@PathVariable String email, @PathVariable String password, Model model){
+        UserCredential userCredential = userCredentialService.getUserByEmailAndPassword(email, password);
+        model.addAttribute("nom_du_model_dans_la_vue", userCredential);
+        return "view";
+    }
 
 }
