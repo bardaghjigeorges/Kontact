007797cf45fb5369472eb3f89ef1cf960276bab8
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-10-21 12:24:06 +0200

    version 1 de controller

--- src/main/java/com/m2idl/kontact/controller/UserCredentialController.java
+++ src/main/java/com/m2idl/kontact/controller/UserCredentialController.java
@@ -1,5 +1,24 @@
 package com.m2idl.kontact.controller;
 
+import com.m2idl.kontact.entity.UserCredential;
+import com.m2idl.kontact.service.UserCredentialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserCredentialController {
+/*
+    @Autowired
+    UserCredentialService userCredentialService;
+
+    @PostMapping("/index/addUser")
+    public UserCredential addNewUser(UserCredential userCredential){
+        userCredentialService.addUser(userCredential);
+        return userCredential;
+    }*/
 
 }
