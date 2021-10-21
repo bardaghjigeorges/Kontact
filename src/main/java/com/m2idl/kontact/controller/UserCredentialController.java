17ec5b6ff62efa8d7d1d71718ad0d73410eb43f4
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2021-10-21 14:02:32 +0200

    modifications dans le controller

--- src/main/java/com/m2idl/kontact/controller/UserCredentialController.java
+++ src/main/java/com/m2idl/kontact/controller/UserCredentialController.java
@@ -3,12 +3,11 @@ package com.m2idl.kontact.controller;
 import com.m2idl.kontact.entity.Contact;
 import com.m2idl.kontact.entity.UserCredential;
 import com.m2idl.kontact.service.UserCredentialService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,39 +18,38 @@ public class UserCredentialController {
     @Autowired
     UserCredentialService userCredentialService;
 
-    @RequestMapping("/index/addUser")
-    public String addNewUser(UserCredential userCredential, Model model){
+    @PostMapping("/index/addUser")
+    public UserCredential addNewUser(UserCredential userCredential, Model model){
         userCredentialService.addUser(userCredential);
         model.addAttribute( "no,m_du_model_dans_la_vue", userCredential);
-        return "add_user_View";
+        return userCredential;
     }
 
-    @RequestMapping("/index/user/{email}/{password}")
-    public String findUserOptional(@PathVariable String email, @PathVariable String password, Model model ){
+    @GetMapping("/index/user/{email}/{password}")
+    public Optional<UserCredential> findUserOptional(@PathVariable String email, @PathVariable String password, Model model ){
         // utilisation de l'interface userCredentialService
         Optional<UserCredential> userCredential = userCredentialService.findOptionalUserByEmailAndPassword(email, password);
         // Mettre la valeur de l'utilsateur recuperé dans un attribut model pour pouvoir l'affiché après dans la view
         model.addAttribute( "nom_du_model_dans_la_vue", userCredential);
-        return "view";
+        return userCredential;
     }
 
-    @RequestMapping("/index/user/{email}/{password}")
-    public String findUser(@PathVariable String email, @PathVariable String password, Model model){
+    @GetMapping("/index/user/{email}/{password}")
+    public UserCredential findUser(@PathVariable String email, @PathVariable String password, Model model){
         // utilisation de l'interface userCredentialService
         UserCredential userCredential = userCredentialService.getUserByEmailAndPassword(email, password);
         // Mettre la valeur de l'utilsateur recuperé dans un attribut model pour pouvoir l'affiché après dans la view
         model.addAttribute("nom_du_model_dans_la_vue", userCredential);
-        return "view";
+        return userCredential;
     }
 
-    @RequestMapping("indes/user/contact")
-    public String addAContactToUser(UserCredential userCredential, Contact contact, Model model){
+    @PostMapping("indes/user/contact")
+    public void addAContactToUser(UserCredential userCredential, Contact contact, Model model){
         //ajouter un contact à un user avec la userServiceCrednetial on ne renvoie pas de model on ajoute juste
         userCredentialService.addContactToUser(userCredential, contact);
-        return "View";
     }
 
-    @RequestMapping("/index/user/{email}")
+    @GetMapping("/index/user/{email}")
     public boolean verifyUserByEmail(@PathVariable String email){
         //on verifie via la pathVariable que l'itilisateur existe bien
         if(userCredentialService.userExistsByEmail(email))
@@ -59,14 +57,20 @@ public class UserCredentialController {
         return false;
     }
 
-    @RequestMapping("/indes/user/contact/{email}")
-    public String getAllContactsOfUser (@PathVariable String email, Model model){
+    @GetMapping("/index/user/contact/{email}")
+    public List<Contact> getAllContactsOfUser (@PathVariable String email, Model model){
         //je stock tous les contacts dans une list
         List<Contact> allContacts = userCredentialService.getContactsOfUser(email);
         //j'ajoute une model attribute qui contient tous les contacts
         model.addAttribute("ListContacts", allContacts);
-        return "view";
+        return allContacts;
     }
+
+    @RequestMapping("/index/user/contact/{id}")
+    public void deleteContact(@PathVariable String id, UserCredential userCredential, Contact contact){
+        userCredentialService.deleteContact(userCredential, contact);
+    }
+
 
 
 
