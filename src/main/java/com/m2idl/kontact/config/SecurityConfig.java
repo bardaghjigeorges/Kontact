29bcdd7e2b1381f1aba137e299964d41761de55d
belHak <hak721@outlook.fr>
2021-10-22 20:47:43 +0200

    custom loginPage configuration

--- src/main/java/com/m2idl/kontact/config/SecurityConfig.java
+++ src/main/java/com/m2idl/kontact/config/SecurityConfig.java
@@ -8,12 +8,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableGlobalMethodSecurity(prePostEnabled=true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserAuthenticationService userAuthenticationService ;
+    UserAuthenticationService userAuthenticationService;
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userAuthenticationService);
@@ -21,13 +21,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/register").permitAll()
+        http.authorizeRequests().antMatchers("/register", "/loginPage").permitAll()
                 .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .formLogin()
+                .formLogin().loginPage("/loginPage")
                 .and()
-                .logout();
+                .logout()
+                .and()
+                .headers().frameOptions().disable();
+
     }
 
 }
