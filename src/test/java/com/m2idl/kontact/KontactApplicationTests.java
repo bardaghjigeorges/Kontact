210125c9d2dfeccccb787290639553b0e7c1ce72
SalemDoub <salem.doub2016@etu.univ-amu.fr>
2021-10-23 21:02:21 +0200

    test addContact(ContactServiceImp)

--- src/test/java/com/m2idl/kontact/KontactApplicationTests.java
+++ src/test/java/com/m2idl/kontact/KontactApplicationTests.java
@@ -1,13 +1,39 @@
 package com.m2idl.kontact;
 
+import com.m2idl.kontact.entity.Contact;
+import com.m2idl.kontact.service.ContactServiceImp;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class KontactApplicationTests {
 
+	@Autowired
+	ContactServiceImp contactServiceImp;
+
 	@Test
-	void contextLoads() {
+	@Transactional
+	void testAddContact() {
+		Contact contact = new Contact();
+		contact.setFirstName("salem");
+		contact.setLastName("salem");
+		contact.setTelephone("111111");
+		contact.setId(4);
+		contactServiceImp.addContact(contact);
+
+		Contact expected=contactServiceImp.getContact(4);
+		assertEquals(expected, contact);
 	}
+
+
+
+
+
 
 }
