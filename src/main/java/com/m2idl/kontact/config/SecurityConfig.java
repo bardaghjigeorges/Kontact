3e6e39c3171f966c0ed0d7270c4e9efa7ea17f11
Thibaut <thi6aut@yahoo.fr>
2021-10-23 19:20:47 +0200

    edit antMatchers (SecutityConfig) for URL loginPage register

--- src/main/java/com/m2idl/kontact/config/SecurityConfig.java
+++ src/main/java/com/m2idl/kontact/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/register", "/loginPage").permitAll()
+        http.authorizeRequests().antMatchers("/register", "/loginPage").anonymous()
                 .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
