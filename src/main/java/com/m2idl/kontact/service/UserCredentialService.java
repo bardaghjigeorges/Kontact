1e3e9c6c5586600399e04ccad26e7e259390d778
Thibaut <thi6aut@yahoo.fr>
2021-10-24 17:19:15 +0200

    adding Exception gestion

--- src/main/java/com/m2idl/kontact/service/UserCredentialService.java
+++ src/main/java/com/m2idl/kontact/service/UserCredentialService.java
@@ -1,13 +1,14 @@
 package com.m2idl.kontact.service;
 import com.m2idl.kontact.entity.Contact;
 import com.m2idl.kontact.entity.UserCredential;
+import org.springframework.security.acls.model.AlreadyExistsException;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserCredentialService {
 
-    UserCredential addUser(UserCredential userCredential) throws Exception;
+    UserCredential addUser(UserCredential userCredential) throws AlreadyExistsException;
 
     UserCredential updateUser(UserCredential userCredential) throws Exception;
 
